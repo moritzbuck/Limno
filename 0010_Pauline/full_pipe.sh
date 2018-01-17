6a7ebf692f18f84adc45051ff62c88b4a2ebfb97
Moritz Buck <mortz.buck@nbis.com>
2018-01-17 15:47:34 +0100

    nothing

--- 0010_Pauline/full_pipe.sh
+++ 0010_Pauline/full_pipe.sh
@@ -3,7 +3,7 @@ DIR=$HOME/people/0010_Pauline
 cd
 cd $DIR
 
-QSCRIPT="sbatch -D $DIR -A snic2017-7-173 -t 3-00:00:00 -n 20 -p node --mail-user murumbii@gmail.com --mail-type=ALL"
+QSCRIPT="sbatch -D $DIR -A snic2017-7-173 -t 5-00:00:00 -n 20 -p node --mail-user murumbii@gmail.com --mail-type=ALL"
 
 all_samples=`find 0000_raws/0100_reads/* -type "d" | grep -v FASTQ | rev | cut -f1 -d"/" | rev | sed 's/^/1000_processed_reads\//' | sed 's/$/\/done/' `
 snakemake  --cores 16 $all_samples
